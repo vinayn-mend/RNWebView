76fb3f76c0d24e363b14bac7c62d18f41ed8bcef
Sayone Technologies <sayonetechnologies@Sayones-Mac-mini.local>
2022-08-05 15:02:39 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect, useState} from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, StyleSheet, Text, PermissionsAndroid} from 'react-native';
+import WebView from 'react-native-webview';
+import RNPermissions, {PERMISSIONS} from 'react-native-permissions';
+
+const App: () => Node = () => {
+  const [url, setUrl] = useState('https://portal-dev1.mendvip.com/');
+  const [granted, setGranted] = useState(false);
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  const requestPermissions = async () => {
+    await RNPermissions.requestMultiple([
+      PERMISSIONS.ANDROID.CAMERA,
+      PERMISSIONS.ANDROID.RECORD_AUDIO,
+      PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+      PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+    ]).then(statuses => {
+      console.log('Status: ', statuses);
+      setGranted(true);
+    });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Website not loaded</Text>
+      {granted && (
+        <WebView
+          // originWhitelist={['*']}
+          source={{uri: 'https://portal-dev1.mendvip.com/video/MRCPXZ'}} // <--- That url is set here
+          javaScriptEnabled={true}
+          onNavigationStateChange={navigationState => {}}
+          allowsInlineMediaPlayback={true}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
